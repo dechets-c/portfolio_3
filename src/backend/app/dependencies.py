9153ee81974bc3b5a67da0ae72d7cf9b142081ba
dechets-c <q.delrive@dechet.ovh>
2026-04-09 16:40:26 +0200

    Ajout connexion bdd

--- src/backend/app/dependencies.py
+++ src/backend/app/dependencies.py
@@ -1,2 +1,12 @@
 # Contient les infos partagées entre les apps
 # Genre les sessions de databases
+
+from app.db.database import SessionLocal
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
