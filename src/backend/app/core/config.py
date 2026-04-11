700ea66d2f1c07bfb4a58be4e8a1d59b744be2b5
dechets-c <q.delrive@dechet.ovh>
2026-04-11 17:21:53 +0200

    Ajout du chargement des var env

--- src/backend/app/core/config.py
+++ src/backend/app/core/config.py
@@ -1,5 +1,4 @@
 import os
-import ssl
 
 from dotenv import load_dotenv
 from sqlalchemy import URL
@@ -11,17 +10,15 @@ db_host = os.getenv("db_host")
 db_user = os.getenv("db_user")
 db_password = os.getenv("db_password")
 db_port = os.getenv("db_port")
-db_ssl = os.getenv("db_ssl")
 
 
 DATABASE_URL = URL.create(
-    "postgresql+pg8000",
+    "postgresql+psycopg2",
     username=db_user,
     password=db_password,
     host=db_host,
     database=db_name,
     port=db_port,
-    ssl=db_ssl,
 )
 
 SECRET_KEY = os.getenv("secret_key")
