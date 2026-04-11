ce53c8a4df3687580f30c1b3680c1fdab4d6766b
dechets-c <q.delrive@dechet.ovh>
2026-04-11 17:23:20 +0200

    Ajout de l'insert dans la base de données des projets

--- src/backend/app/routers/admin.py
+++ src/backend/app/routers/admin.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter, Depends  # depends pour ce qui est dans dependencies
+from sqlalchemy.orm import Session
+
+from app.dependencies import get_db
+from app.schemas import ProjectCreate
+from app import models
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 
-@router.post("/insert")
-def insert_to_db():
-    pass
+@router.post("/create_project")
+def create_project(data: ProjectCreate, db: Session = Depends(get_db)):
+    new_project = models.Project(**data.model_dump())
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+
+    return {"message": "Projet inséré avec succès", "id": new_project.id}
 
 
 @router.post("/delete")
